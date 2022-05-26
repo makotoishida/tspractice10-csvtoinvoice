3a5a15a29ae35abcab155eff4a6c141c6e2b6572
Makoto Ishida <70257274+makotoishida@users.noreply.github.com>
2022-05-25 20:19:29 -1000

    Read file content.

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,41 @@ import './style.css'
 
 const appRoot = document.querySelector<HTMLDivElement>('#app')!
 
+
+const readFileAsText = (file: File): Promise<string> => {
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader()
+    reader.onerror = () => reject(reader.error)
+    reader.onload = () => resolve((reader.result as string) || '')
+    reader.readAsText(file)
+  })
+}
+
+const mapCSVToArray = (csv: string): string[][] => {
+  return csv
+    .split('\n')
+    .filter((row) => row.length > 0)
+    .map((row) => row.split(','))
+}
+
+const handleSelectFile = async (ev: InputEvent) => {
+  const input = ev.target as HTMLInputElement
+  const files = input.files as FileList
+  if (!files || files.length === 0) {
+    return
+  }
+
+  const file = files[0]
+  const str = await readFileAsText(file)
+  const arr = mapCSVToArray(str)
+  console.log(arr)
+}
+
 const App = () => {
-  return html`<div>CSV TO INVOICE</div>`
+  return html`<div>
+    <p>Please select a CSV file</p>
+    <input type="file" @change=${handleSelectFile} accept="text/csv" />
+  </div>`
 }
 
 const renderApp = () => {
